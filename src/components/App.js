a2792de940255dcc09a0f3186cbcbf644b03a647
Ken Hendricks <kenhendricks97@gmail.com>
2019-09-05 10:25:05 +1200

    setting up different pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'; 
+import {BrowserRouter, Route, Link} from 'react-router-dom'; 
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 
-const PageOne = () => {
-    return <div>PageOne</div>
-};
 
-const PageTwo = () => {
-    return <div>PageTwo</div>
-};
 
 
 const App = () => {
@@ -15,12 +14,16 @@ const App = () => {
     <div> 
         <BrowserRouter>
             <div>
-            <Route path="/" exact component={PageOne} />
-            <Route path="/pagetwo" component={PageTwo} />
+                <Route path="/" exact component={StreamList} /> {/* the home path*/}
+                <Route path="/streams/new" exact component={StreamCreate} /> 
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
             </div>
         </BrowserRouter>
     </div>
-
+    // if you write exact like above its like exact ={true}
+    // exact will mean, does it exactly match the path 
     );
 
 };
