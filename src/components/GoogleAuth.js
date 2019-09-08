7c92aeac7221f3994f2bcb0343bf86413cc2089e
Ken Hendricks <kenhendricks97@gmail.com>
2019-09-08 23:51:55 +1200

    rendering the auth status on screen

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = {isSignedIn: null}; //dont know if signed in or not at start
+
     //want to initalise only once when first rendered
     componentDidMount( ) {
         window.gapi.load('client:auth2', () => {
-            //takes some time to reach google servers, need call back once loaded
+            //asynchronous - takes some time to reach google servers, need call back once loaded
             window.gapi.client.init({
                 clientId: '632603483086-ieauatod2hrjd4lc4oi0sj6rd6n2tms2.apps.googleusercontent.com',
                 //what we want from the client
                 scope: 'email'
 
+            }).then(() => { //init is a promise - when library initialises it will run .then
+                //can sign in, sign out and print auth status
+                this.auth = window.gapi.auth2.getAuthInstance();
+                //gets status of sign in
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
             });
         });
     }
     
+    //helper method- check sign in
+    renderAuthButton() {
+        if(this.state.isSignedIn === null) {
+            return <div> i dont know </div>
+        } else if (this.state.isSignedIn) { //true
+            return <div> Signed In </div>
+        } else { //false
+            return <div> Signed Out </div>
+        } 
+    }
+
     render() {
-        return <div> Google Auth </div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
 
