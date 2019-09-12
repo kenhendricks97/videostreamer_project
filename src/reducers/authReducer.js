b1cd939a8a7276695d32a02a475ab8397e2b8686
Ken Hendricks <kenhendricks97@gmail.com>
2019-09-13 03:33:29 +1200

    auth reducers

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,19 @@
+//initialize state of sign in - initial_state is constant object - do not chage - capitals
+const INITIAL_STATE = {
+    isSignedIn: null
+};
+
+//when app first boots, reducer called 1 time to initial it,
+//if the user is called with first value of undefined
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SIGN_IN':
+            //update and modify state
+            return {...state,isSignedIn: true};
+        case 'SIGN_OUT':
+            //update and modify state
+            return {...state,isSignedIn: false};
+        default:
+            return state;
+    }
+};
