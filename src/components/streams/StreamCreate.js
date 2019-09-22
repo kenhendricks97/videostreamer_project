ba2e5e7f87057ff25dab526a59a0a35bc0db2fde
Ken Hendricks <kenhendricks97@gmail.com>
2019-09-22 19:25:03 +1200

    saving inputs for fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,12 +4,22 @@ import {Field,reduxForm} from 'redux-form';
 
 
 class StreamCreate extends React.Component {
+    //helper method
+    renderInput(formProps) {
+        console.log(formProps)
+        return <input 
+        onChange={formProps.input.onChange} 
+        value={formProps.input.value}
+        />
+        
+    }
+
     render() {
        // console.log(this.props); 
         return (
             <form>
-                <Field name="Title" />
-                <Field name= "Description" />
+                <Field name="Title" component={this.renderInput}/> 
+                <Field name= "Description" component={this.renderInput}/>
             </form>
         );
     }
