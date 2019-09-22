b44b3c1233496794c450d3d860e0bf034c9a0c76
Ken Hendricks <kenhendricks97@gmail.com>
2019-09-22 19:07:30 +1200

    adding stream create form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Field,reduxForm} from 'redux-form'; 
 
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
 
-};
+class StreamCreate extends React.Component {
+    render() {
+       // console.log(this.props); 
+        return (
+            <form>
+                <Field name="Title" />
+                <Field name= "Description" />
+            </form>
+        );
+    }
+}
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form'; //named import and renamed formreducer
 import authReducer from './authReducer';
 
 export default combineReducers ( {
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer //assign to key and coming from formReducer
 });
