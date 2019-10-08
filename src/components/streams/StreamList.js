807cedc60477309a1af22a0cd0be972769af9231
Ken Hendricks <kenhendricks97@gmail.com>
2019-10-09 10:08:35 +1300

    making sure stream list appears from api

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {connect } from 'react-redux';
+import {fetchStreams} from '../../actions';
 
-
-const StreamList = () => {
+/*const StreamList = () => {
     return <div>StreamList</div>
 
-};
+};*/
 
-export default StreamList;
+//use class based component as we only want to call the list once every time it loads
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    render(){
+        return <div>StreamList</div>;
+    }
+}
+
+export default connect(null,{fetchStreams})(StreamList);
